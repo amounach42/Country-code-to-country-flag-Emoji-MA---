6040f1ea841da451cc2b470eb4b621bff10a824b
karroch-a <abdellah.azeroual.info@gmail.com>
2023-07-10 14:26:48 +0100

    Country code to country flag Emoji

---
+++ flag.js
@@ -0,0 +1,11 @@
+// Converts country code (ISO 3166-1) to its emoji flag representation (MA -> 🇲🇦).
+// 0x1F1E5 =>  Base code point for flag emojis
+
+const countryFlags = (countryCode) =>
+  Array.from(countryCode, (letter) =>
+    String.fromCodePoint((letter.codePointAt() % 32) + 0x1f1e5)
+  ).join("");
+
+//console.log(countryFlags("MA")) =>  🇲🇦
+//console.log(countryFlags("US")) =>  🇺🇸
+//console.log(countryFlags("MA")) =>  🇬🇧
